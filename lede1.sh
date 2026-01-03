3a3388a3bfd058bd6adcf0f26653b45472506c02
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-01-03 18:57:40 +0800

    Update lede1.sh

--- lede1.sh
+++ lede1.sh
@@ -7,3 +7,18 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+
+
+
+
+#加入nano
+git clone --depth=1 \
+  https://github.com/openwrt/packages.git \
+  -b master \
+  /tmp/packages
+
+mkdir -p package/custom
+cp -r /tmp/packages/utils/nano package/custom/
+rm -rf /tmp/packages
